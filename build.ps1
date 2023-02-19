53df1859a4ab6843b424ec5760c30a77d74066dd
Simona Avornicesei <simona@avornicesei.com>
2023-02-19 03:58:38 +0200

    Fix win build due to missing pwsh and missing path to csc

--- build.ps1
+++ build.ps1
@@ -310,6 +310,21 @@ Enter-Build {
     Build configuration - .net runner: $($BUILD_CONFIGURATION.DotNetRunner),
     Build configuration - resgen executable: $($BUILD_CONFIGURATION.ResgenExe),
     Build configuration - compiler debug flag: $($BUILD_CONFIGURATION.CompilerDebug)"
+
+    if($IsWindows)
+    {
+        #set environment variables from VsDevCmd without creating a nested prompt 
+        $installationPath = (Join-Path $TOOLS_DIR 'vswhere.exe') -prerelease -latest -property installationPath
+        if ($installationPath -and (Test-Path "$installationPath\Common7\Tools\vsdevcmd.bat")) {
+        & "${env:COMSPEC}" /s /c "`"$installationPath\Common7\Tools\vsdevcmd.bat`" -no_logo && set" | foreach-object {
+            $name, $value = $_ -split '=', 2
+            set-content env:\"$name" $value
+            }
+        }
+
+        exec { & where resgen }
+        exec { & where csc }
+    }
 }
 
 #####################################
